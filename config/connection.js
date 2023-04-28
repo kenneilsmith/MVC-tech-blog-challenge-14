39fcf74c2573c13466cf5a9567d5879012628731
DAMIAN SMITH <damiansmith@DAMIANs-Mac-mini.local>
2023-04-28 13:26:55 -0400

    fixed PORT error

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const db = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
-    // host: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 })
 
